bc4601e856c48704f3a2f2c7bb28b08a268a97b3
mahonya333 <mahonya333@gmail.com>
2020-04-20 23:27:36 +0300

    сверстана секция team

--- docs/js/main.js
+++ docs/js/main.js
@@ -28,7 +28,30 @@ $(function () {
             }, 1000);
         }
     }
-
+    //Функция отвечает за планвый переход по якорным ссылкам
+  $(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      // отменяем стандартное действие
+      event.preventDefault();
+      
+      var scrollToBlock = $(this).attr("href"),
+          blockPosition = $(scrollToBlock).offset().top;
+      /*
+      scrollToBlock - в переменную заносим информацию о том, к какому блоку надо перейти
+      blockPosition - определяем положение блока на странице
+      */ 
+      $('html, body').animate({scrollTop: blockPosition}, 2000); 
+      // 2000 скорость перехода в миллисекундах
+    });
+  });
+  $('.team-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    variableWidth: true,
+  });
 
 
 });

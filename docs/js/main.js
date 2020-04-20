26c261a1710f9ac78933829bed0a268ffa29d306
mahonya333 <mahonya333@gmail.com>
2020-04-20 22:04:31 +0300

    подключены шрифты, сделан header-top, сделана секция about, добавлен animation.scss, правки глоабальных стилей, добавлены картинки

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,34 @@
-$(function(){
+$(function () {
+    $("html,body").css("overflow", "hidden");
+    var
+        images = document.images,
+        imagesTotalCount = images.length,
+        imagesLoadedCount = 0;
+    percDisplay = document.getElementById('load-border');
+    for (var i = 0; i < imagesTotalCount; i++) {
+        imageClone = new Image();
+        imageClone.onload = imageLoaded;
+        imageClone.onerror = imageLoaded;
+        imageClone.src = images[i].src;
+    }
+    //функция полосы загрузки в зависимости от количества загруженных картинок с минимальным временем в 1 секунду
+    function imageLoaded() {
+        imagesLoadedCount++;
+        percDisplay.style.width = (((100 / imagesTotalCount) * imagesLoadedCount) << 0) + '%';
+        if (imagesLoadedCount >= imagesTotalCount) {
+            setTimeout(function () {
+                var preloader = document.getElementById('page-preloader');
+                /*   var loader = document.getElementById('loader'); */
+                if (!preloader.classList.contains('done')) {
+                    preloader.classList.add('done');
+                    $("html,body").css("overflow", "visible");
+                    $('.preloader').css('background-image', 'none');
+                    /* loader.classList.add('loader-static'); */
+                }
+            }, 1000);
+        }
+    }
 
 
 
-  
 });
